8f5db4dfa04d946fa36f545f7376bd24882b8faa
Elton Souza <elton.souza1515@gmail.com>
2021-03-23 11:53:07 -0300

    Update index.js

--- Java Script/JS-00/index.js
+++ Java Script/JS-00/index.js
@@ -5,9 +5,9 @@ const cliente1 = new Cliente()
 cliente1.nome="Elton"
 cliente1.cpf=11212391209
 
-//const cliente2 = new Cliente()
-//cliente2.nome="Souza"
-//cliente2.cpf=12365478901
+const cliente2 = new Cliente()
+cliente2.nome="Souza"
+cliente2.cpf=12365478901
 
 const conta1 = new ContaCorrente()
 conta1.agencia = 1101
@@ -16,22 +16,11 @@ conta1.depositar(500)
 
 const conta2 = new ContaCorrente()
 conta2.agencia = 1101
-conta2.cliente = new Cliente()
-conta2.cliente.nome = "Souza"
-conta2.cliente.cpf = 12365478901
+conta2.cliente = cliente2;
 
 conta1.transferir(100,conta2)
 console.log(conta1)
 console.log(conta2)
-
-
-
-
-
-
-
-
-
 
 
 
