c0de5c826afcff1218001b40409c9de1b6ece97e
Elton Souza <elton.souza1515@gmail.com>
2021-04-05 20:30:04 -0300

    Atualizando codigo do banco.
    utilizando assessores Get e Set;
    construtores;
    variáveis estáticas;

--- Java Script/JS-00/ContaCorrente.js
+++ Java Script/JS-00/ContaCorrente.js
@@ -1,11 +1,31 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente{
-    cliente;
+    static numeroDeContas = 0
+    _cliente;
     agencia;
-
-
-    //#saldo = 0 https://github.com/tc39/proposal-class-fields#private-fields
     _saldo = 0;
-    
+
+    set cliente(novoCliente){
+        if (novoCliente instanceof Cliente){
+            this._cliente = novoCliente
+        }
+    }
+    get cliente(){
+        return this._cliente
+    }
+
+    get saldo (){
+        return this._saldo
+    }
+
+    constructor(cliente, agencia){
+        this.agencia = agencia
+        this.cliente = cliente
+        ContaCorrente.numeroDeContas += 1
+    }
+
+
     sacar(valor){
         if(this._saldo >= valor){
             this._saldo -= valor
--- Java Script/JS-00/index.js
+++ Java Script/JS-00/index.js
@@ -1,27 +1,18 @@
 import{Cliente} from "./Cliente.js"
 import{ContaCorrente} from "./ContaCorrente.js"
 
-const cliente1 = new Cliente()
-cliente1.nome="Elton"
-cliente1.cpf=11212391209
+const cliente1 = new Cliente("Elton", 11212391209)
 
-const cliente2 = new Cliente()
-cliente2.nome="Souza"
-cliente2.cpf=12365478901
+const cliente2 = new Cliente("Souza", 12365478901 )
 
-const conta1 = new ContaCorrente()
-conta1.agencia = 1101
-conta1.cliente = cliente1
+const conta1 = new ContaCorrente(cliente1, 1101)
 conta1.depositar(500)
 
-const conta2 = new ContaCorrente()
-conta2.agencia = 1101
-conta2.cliente = cliente2;
-
-conta1.transferir(200,conta2)
-console.log(conta1)
-console.log(conta2)
+const conta2 = new ContaCorrente(cliente2, 1101)
 
 
+// conta1.transferir(200,conta2)
+// console.log(conta1)
+// console.log(conta2)
 
-
+console.log(ContaCorrente.numeroDeContas)
