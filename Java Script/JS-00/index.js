ddce2af07c567d40c8f49cd9250547ecdf78fb6b
Elton Souza <elton.souza1515@gmail.com>
2021-04-06 15:51:21 -0300

    Update index.js

--- Java Script/JS-00/index.js
+++ Java Script/JS-00/index.js
@@ -6,13 +6,5 @@ const cliente1 = new Cliente("Elton", 11212391209)
 const cliente2 = new Cliente("Souza", 12365478901 )
 
 const conta1 = new ContaCorrente(cliente1, 1101)
-conta1.depositar(500)
 
 const conta2 = new ContaCorrente(cliente2, 1101)
-
-
-// conta1.transferir(200,conta2)
-// console.log(conta1)
-// console.log(conta2)
-
-console.log(ContaCorrente.numeroDeContas)
