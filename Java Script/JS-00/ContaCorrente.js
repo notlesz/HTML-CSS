04c19f818b1d64abf69e12797dd26f3480df0b08
Elton Souza <elton.souza1515@gmail.com>
2021-04-07 10:54:20 -0300

    Update ContaCorrente.js

--- Java Script/JS-00/ContaCorrente.js
+++ Java Script/JS-00/ContaCorrente.js
@@ -1,46 +1,13 @@
-import { Cliente } from "./Cliente.js";
+import { Conta } from "./Conta.js";
 
-export class ContaCorrente{
+export class ContaCorrente extends Conta{
     static numeroDeContas = 0
-    _cliente;
-    agencia;
-    _saldo = 0;
-
-    set cliente(novoCliente){
-        if (novoCliente instanceof Cliente){
-            this._cliente = novoCliente
-        }
-    }
-    get cliente(){
-        return this._cliente
-    }
-
-    get saldo (){
-        return this._saldo
-    }
-
     constructor(cliente, agencia){
-        this.agencia = agencia
-        this.cliente = cliente
+        super(0, cliente, agencia)
         ContaCorrente.numeroDeContas += 1
     }
-
-
     sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor
-            return valor
-        }    
+        let taxa = 1.1
+        return this._sacar(valor, taxa)   
     }
-    depositar(valor){
-        if(valor <= 0){
-           return
-        }
-        this._saldo += valor
-    }
-    transferir(valor,conta){
-        const valorSacado = this.sacar(valor)
-        conta.depositar(valorSacado)
-    }
-
 }
