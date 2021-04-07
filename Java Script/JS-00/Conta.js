408cf0318005a3b84f481ee37ca489e0080e61c1
Elton Souza <elton.souza1515@gmail.com>
2021-04-07 10:54:02 -0300

    Create Conta.js

---
+++ Java Script/JS-00/Conta.js
@@ -0,0 +1,47 @@
+export class Conta{
+    constructor(saldoInicial, cliente, agencia){
+        this._saldo = saldoInicial
+        this._cliente = cliente
+        this._agencia = agencia
+    }
+
+    set cliente(novoCliente){
+        if (novoCliente instanceof Cliente){
+            this._cliente = novoCliente
+        }
+    }
+
+    get cliente(){
+        return this._cliente
+    }
+
+    get saldo (){
+        return this._saldo
+    }
+
+    sacar(valor){
+        let taxa = 1 
+        return this._sacar(valor, taxa)  
+    }
+
+    _sacar(valor, taxa){
+        const valorSacado = taxa * valor
+        if(this._saldo >= valorSacado){
+            this._saldo -= valorSacado
+            return valorSacado
+        } 
+        return 0
+    }
+
+    depositar(valor){
+        if(valor <= 0){
+           return
+        }
+        this._saldo += valor
+    }
+    
+    transferir(valor,conta){
+        const valorSacado = this.sacar(valor)
+        conta.depositar(valorSacado)
+    }
+}
