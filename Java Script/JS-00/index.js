5dbed9f96b18b68ca76822a21ffd981d2acb7584
Elton Souza <elton.souza1515@gmail.com>
2021-04-12 16:10:11 -0300

    Update index.js

--- Java Script/JS-00/index.js
+++ Java Script/JS-00/index.js
@@ -1,12 +1,16 @@
 import{Cliente} from "./Cliente.js"
 import{ContaCorrente} from "./ContaCorrente.js"
 import {ContaPoupanca} from "./ContaPoupanca.js"
+
+
 const cliente1 = new Cliente("Elton", 11212391209)
 
 const contaCC = new ContaCorrente(cliente1, 1101)
 contaCC.depositar(500)
 
 const contaPP = new ContaPoupanca(50, cliente1, 1101)
+
+
 
 contaCC.sacar(100)
 contaPP.sacar(10)
