36ab19c1fa3e381456a8e2f69f1c585941eba68d
Elton Souza <elton.souza1515@gmail.com>
2021-04-12 16:10:37 -0300

    Update ContaCorrente.js

--- Java Script/JS-00/ContaCorrente.js
+++ Java Script/JS-00/ContaCorrente.js
@@ -6,6 +6,8 @@ export class ContaCorrente extends Conta{
         super(0, cliente, agencia)
         ContaCorrente.numeroDeContas += 1
     }
+
+    //sobrescrevendo metodos
     sacar(valor){
         let taxa = 1.1
         return this._sacar(valor, taxa)   
