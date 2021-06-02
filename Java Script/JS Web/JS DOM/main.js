23b6f4ded1756190967cf6d7c05d99f6938a4204
Elton Souza <elton.souza1515@gmail.com>
2021-06-02 10:56:19 -0300

    adicionando horário

--- Java Script/JS Web/JS DOM/main.js
+++ Java Script/JS Web/JS DOM/main.js
@@ -1,26 +1,40 @@
 import BotaoConclui from './componentes/concluiTarefa.js'
 import BotaoDeleta from './componentes/deletaTarefa.js'
 
-    const criarTarefa = (evento) => {
-        evento.preventDefault()
-    
-        const input = document.querySelector('[data-form-input]')
-        const valor = input.value
+//Função para trabalhar com os dados
+const handleNovoItem = (evento) =>{
+    evento.preventDefault()
 
-        const lista = document.querySelector('[data-list]')
-    
-        const tarefa = document.createElement('li')
-        tarefa.classList.add('task')
+    const lista = document.querySelector('[data-list]')
+    const input = document.querySelector('[data-form-input]')
+    const valor = input.value
 
-        const conteudo = `<p class="content">${valor}</p>`
-        tarefa.innerHTML = conteudo
-        lista.appendChild(tarefa)
+    const calendario = document.querySelector('[data-form-date]')
+    const data = moment(calendario.value)
+    const dataFormatada = data.format('DD/MM/YYYY')
 
-        tarefa.appendChild(BotaoConclui())
-        tarefa.appendChild(BotaoDeleta())
-        input.value = ' '
+    const dados = {
+        valor, dataFormatada
     }
+    const criaTarefa = criarTarefa(dados)
+    lista.appendChild(criaTarefa)
+    input.value = ' '
 
-    const novaTarefa = document.querySelector('[data-form-button]')
+}    
+//Função para trabalhar com a visualização desses dados
+const criarTarefa = ({valor, dataFormatada}) => {
+    const tarefa = document.createElement('li')
+    tarefa.classList.add('task')
 
-    novaTarefa.addEventListener('click', criarTarefa)
+    const conteudo = `<p class="content">${dataFormatada} * ${valor}</p>`
+    tarefa.innerHTML = conteudo
+   
+    tarefa.appendChild(BotaoConclui())
+    tarefa.appendChild(BotaoDeleta())
+
+    return tarefa
+}
+
+const novaTarefa = document.querySelector('[data-form-button]')
+
+novaTarefa.addEventListener('click', handleNovoItem)
