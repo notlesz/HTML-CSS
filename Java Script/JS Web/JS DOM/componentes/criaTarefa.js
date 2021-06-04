ebffe87021c43a1232c1a337b7a67a1d820aabca
Elton Souza <elton.souza1515@gmail.com>
2021-06-04 17:21:13 -0300

    LocalStorage

---
+++ Java Script/JS Web/JS DOM/componentes/criaTarefa.js
@@ -0,0 +1,42 @@
+import BotaoConclui from './concluiTarefa.js'
+import BotaoDeleta from './deletaTarefa.JS'
+//Função para trabalhar com os dados
+export const handleNovoItem = (evento) =>{
+    evento.preventDefault()
+    
+    const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
+    const lista = document.querySelector('[data-list]')
+    const input = document.querySelector('[data-form-input]')
+    const valor = input.value
+
+    const calendario = document.querySelector('[data-form-date]')
+    const data = moment(calendario.value)
+    const dataFormatada = data.format('DD/MM/YYYY')
+
+    const dados = {
+        valor, dataFormatada
+    }
+
+    const tarefasAtualizadas = [...tarefas,dados]
+    console.log(tarefasAtualizadas)
+    const criaTarefa = Tarefa(dados)
+    
+    lista.appendChild(criaTarefa)
+
+    localStorage.setItem('tarefas', JSON.stringify(tarefasAtualizadas) )
+    
+    input.value = ' '
+
+}    
+//Função para trabalhar com a visualização desses dados
+export const Tarefa = ({valor, dataFormatada}) => {
+    const tarefa = document.createElement('li')
+    tarefa.classList.add('task')
+
+    const conteudo = `<p class="content">${dataFormatada} * ${valor}</p>`
+    tarefa.innerHTML = conteudo 
+    tarefa.appendChild(BotaoConclui())
+    tarefa.appendChild(BotaoDeleta())
+
+    return tarefa
+}
--- Java Script/JS Web/JS DOM/main.js
+++ Java Script/JS Web/JS DOM/main.js
@@ -1,46 +1,4 @@
-import BotaoConclui from './componentes/concluiTarefa.js'
-import BotaoDeleta from './componentes/deletaTarefa.js'
-
-let tarefas = []
-//Função para trabalhar com os dados
-const handleNovoItem = (evento) =>{
-    evento.preventDefault()
-
-    const lista = document.querySelector('[data-list]')
-    const input = document.querySelector('[data-form-input]')
-    const valor = input.value
-
-    const calendario = document.querySelector('[data-form-date]')
-    const data = moment(calendario.value)
-    const dataFormatada = data.format('DD/MM/YYYY')
-
-    const dados = {
-        valor, dataFormatada
-    }
-    tarefas.push(dados)
-
-    const criaTarefa = criarTarefa(dados)
-
-    localStorage.setItem('tarefas', JSON.stringify(tarefas) )
-
-    lista.appendChild(criaTarefa)
-    input.value = ' '
-
-
-}    
-//Função para trabalhar com a visualização desses dados
-const criarTarefa = ({valor, dataFormatada}) => {
-    const tarefa = document.createElement('li')
-    tarefa.classList.add('task')
-
-    const conteudo = `<p class="content">${dataFormatada} * ${valor}</p>`
-    tarefa.innerHTML = conteudo
-   
-    tarefa.appendChild(BotaoConclui())
-    tarefa.appendChild(BotaoDeleta())
-
-    return tarefa
-}
+import {handleNovoItem} from './componentes/criaTarefa.js'
 
 const novaTarefa = document.querySelector('[data-form-button]')
 
